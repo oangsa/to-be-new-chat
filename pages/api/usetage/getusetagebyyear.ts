d967774604bdbde7500ef9f4327afccb28615663
oangsa <sukruangkul.aongsa@gmail.com>
2023-07-12 01:49:07 +0700

    d

--- pages/api/usetage/getusetagebyyear.ts
+++ pages/api/usetage/getusetagebyyear.ts
@@ -7,20 +7,20 @@ interface data {
     total: number[]
 }
 export default async function getUsetageByYear(req: NextApiRequest, res: NextApiResponse) {
-    var data: data  = {
-        month: [],
-        total: []
-    }
-    var old = 1
-    var month: number = 0
-    var total: number = 0
-    const result: any = await prisma.student.findMany({
-        orderBy: [
-            {
-                yearClass: "asc"
-            }
-        ],
-    })
+    // var data: data  = {
+    //     month: [],
+    //     total: []
+    // }
+    // var old = 1
+    // var month: number = 0
+    // var total: number = 0
+    // const result: any = await prisma.student.findMany({
+    //     orderBy: [
+    //         {
+    //             yearClass: "asc"
+    //         }
+    //     ],
+    // })
 
     // result.map((item: any) => {
     //     if (item.yearClass !== old) {
@@ -37,24 +37,26 @@ export default async function getUsetageByYear(req: NextApiRequest, res: NextApi
     // data.month.push(month)
     // data.total.push(total)
 
-    for (let i = 0; i < result.length; i++) {
-        const item = result[i];
-        
-        if (item.yearClass !== old) {
-            data.month.push(month);
-            data.total.push(total);
-            month = 0;
-            total = 0;
-            old = old + 1;
-            continue;
-        }
-        
-        month += item.oldMonth;
-        total += item.total;
+    var data: any = {
+        month: [],
+        total: []
     }
-    
-    data.month.push(month);
-    data.total.push(total);
+    for (let i: number = 1; i <= 6; i++) {
+        const result: any = await prisma.student.findMany({
+            where: {
+                yearClass: i
+            }
+        })
+        var month: any = 0
+        var total: any = 0
+        result.map((item:any) => {
+            month = month + item.oldMonth,
+            total = total + item.total
+        })
+        data.month.push(month)
+        data.total.push(total)
+    }
+    // console.log(data)
 
     return res.status(200).send(data)
 }
